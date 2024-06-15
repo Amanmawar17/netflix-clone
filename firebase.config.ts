a18c3edb223f1ab1fc740a8c4ef0e683603ac193
Frieden <amanmawar17141@gmail.com>
2024-06-16 00:24:56 +0530

    changing stack

--- firebase.config.ts
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyB5CsOgxAUsbDWocuKMl_G34VX7H5XTtQ8",
-  authDomain: "sign-in-32085.firebaseapp.com",
-  projectId: "sign-in-32085",
-  storageBucket: "sign-in-32085.appspot.com",
-  messagingSenderId: "325772423814",
-  appId: "1:325772423814:web:c2352db47b081af9979707"
-};
